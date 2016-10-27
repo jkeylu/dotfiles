97f8ab4dce35a72e7f6ec39303b46e6b983d4b45
jKey Lu <jkeylu@gmail.com>
2016-10-27 14:50:38 +0800

    update vim.sh

--- opener/vim.sh
+++ opener/vim.sh
@@ -18,7 +18,8 @@ let mapleader = ','
 colorscheme desert
 imap jj <Esc>`^
 nmap <leader>nt :Explore<CR>
-nmap <leader>q :bd<CR>
+nmap <leader>q :q<CR>
+autocmd FileType netrw nmap q :bd<CR>
 EOF
 }
 
