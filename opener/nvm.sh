68f608e439ea7beb60d2a76230dcc9891d157348
jKey Lu <jkeylu@gmail.com>
2017-05-22 22:41:23 +0800

    add update to opener/nvm.sh

--- opener/nvm.sh
+++ opener/nvm.sh
@@ -6,7 +6,7 @@ help() {
   cat << EOF
 supported commands:
   install
-  install wsl
+  update
 EOF
 }
 
@@ -17,6 +17,14 @@ install() {
   fi
 
   git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+}
+
+update() {
+  export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  ) && . "$NVM_DIR/nvm.sh"
 }
 
 cmd="$(join_by _ "$@")"
