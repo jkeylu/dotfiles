6a36d2d2a7ea173716a552bbe375e9b1323277dd
jKey Lu <jkeylu@gmail.com>
2019-11-25 21:19:13 +0800

    update nvm config

--- opener/nvm.sh
+++ opener/nvm.sh
@@ -17,9 +17,9 @@ install() {
   fi
 
   export NVM_DIR="$HOME/.nvm" && (
-    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
     cd "$NVM_DIR"
-    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
   ) && . "$NVM_DIR/nvm.sh"
 }
 
@@ -31,8 +31,8 @@ update() {
 
   (
     cd "$NVM_DIR"
-    git fetch origin
-    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+    git fetch --tags origin
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
   ) && . "$NVM_DIR/nvm.sh"
 }
 
