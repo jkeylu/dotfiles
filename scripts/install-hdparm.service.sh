cc93105b1668a3df7c5bab7bf7d126a2576a35c8
jKey Lu <jkeylu@gmail.com>
2020-02-16 16:07:52 +0800

    update install-hdparam.service.sh

--- scripts/install-hdparm.service.sh
+++ scripts/install-hdparm.service.sh
@@ -1,42 +1,25 @@
 #!/usr/bin/env bash
 
 # Usage:
-# sudo bash <(wget -O- https://raw.githubusercontent.com/jkeylu/dotfiles/master/scripts/install-hdparam.service.sh) install
-# sudo ./.dotfiles/scripts/install-hdparam.service.sh install
+# wget -O- https://raw.githubusercontent.com/jkeylu/dotfiles/master/scripts/install-hdparam.service.sh | sudo bash
 
-install() {
-  cat > /etc/systemd/system/hdparm.service << EOF
+cat > /etc/systemd/system/hdparm.service << EOF
 [Unit]
 Description=hdparm service
 
 [Service]
 Type=oneshot
-ExecStart=$(which hdparm) -S 241 -y /dev/sda
+ExecStart=$(which hdparm) -S 241 -y /dev/sdx
 RemainAfterExit=true
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-  if command -v vim &> /dev/null; then
-    vim /etc/systemd/system/hdparm.service
-  else
-    vi /etc/systemd/system/hdparm.service
-  fi
+if command -v vim &> /dev/null; then
+  vim /etc/systemd/system/hdparm.service
+else
+  vi /etc/systemd/system/hdparm.service
+fi
 
-  systemctl enable hdparm
-}
-
-uninstall() {
-  systemctl disable hdparm
-  rm -f /etc/systemd/system/hdparm.service
-}
-
-case "$1" in
-  install)
-    install
-    ;;
-  uninstall)
-    uninstall
-    ;;
-esac
+systemctl enable hdparm
